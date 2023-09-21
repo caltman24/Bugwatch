f5380cb196c8655c30ed756226b3800cf69e547a
Corbyn Altman <corbyn.altman01@gmail.com>
2023-09-20 21:54:56 -0400

    add jwt bearer auth

--- BugwatchApi/Bugwatch.Api/Program.cs
+++ BugwatchApi/Bugwatch.Api/Program.cs
@@ -1,20 +1,56 @@
 using Bugwatch.Api.Extensions;
-using Bugwatch.Api.Filters;
 using Bugwatch.Api.Modules;
 using Bugwatch.Application;
-using Bugwatch.Application.Constants;
 using Bugwatch.Infrastructure;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplication()
     .AddInfrastructure(builder.Configuration);
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer();
+
+// TODO: REMOVE all authId queryStrings from all endpoints. Migrate to Authorization
+builder.Services.AddAuthorization(opts =>
+{
+    opts.DefaultPolicy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .RequireClaim("sub")
+        .Build();
+});
+
 builder.Services.AddResponseCompressionService();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opts =>
+{
+    opts.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = JwtBearerDefaults.AuthenticationScheme,
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header
+    });
+    opts.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
@@ -27,9 +63,6 @@ if (app.Environment.IsDevelopment())
 app.UseProjectModule()
     .UseTeamModule()
     .UseTicketModule();
-
-app.MapGet("/test/rolefilter", ([FromQuery] string authId) => Results.Ok("Test endpoint"))
-    .WithMemberRole(UserRoles.Developer, UserRoles.Admin, UserRoles.ProjectManager);
 
 app.UseResponseCompression();
 app.Run();
